fd19254f77e822cd842b9fa68c957f7645d8528b
HammerPanzer92 <romdur59@gmail.com>
2024-10-23 14:13:28 +0200

    Feat : création d'un DataTable pour la liste des employés (via react-data-table-component)

--- src/components/ListEmployee.jsx
+++ src/components/ListEmployee.jsx
@@ -1,24 +1,66 @@
 import { Link } from "react-router-dom";
 import { useEmployeeStore } from "../services/store";
+import DataTable from 'react-data-table-component';
 
 export function ListEmployee() {
   const employeeList = useEmployeeStore((state) => state.listEmployee);
 
   console.log(employeeList);
 
-  /*
-  Fonction éventuellement utile pour tableau
-  
-  function splitByCapitalLetters(str) {
-    return str.replace(/([A-Z])/g, ' $1').trim();
-  }
-  */
+  const columns = [
+    {
+      name: "First Name",
+      selector: (row) => row.firstname,
+    },
+    {
+      name: "Last Name",
+      selector: (row) => row.lastname,
+      sortable: true,
+    },
+    {
+      name: "Date of Birth",
+      selector: (row) => row.dateOfBirth,
+      sortable: true,
+    },
+    {
+      name: "Start Date",
+      selector: (row) => row.startDate,
+      sortable: true,
+    },
+    {
+      name: "Street",
+      selector: (row) => row.street,
+    },
+    {
+      name: "City",
+      selector: (row) => row.city,
+      sortable: true
+    },
+    {
+      name: "State",
+      selector: (row) => row.state,
+      sortable: true
+    },
+    {
+      name: "Zip Code",
+      selector: (row) => row.zipCode,
+    },
+    {
+      name: "Department",
+      selector: (row) => row.department,
+      sortable: true
+    },
+  ];
 
   return (
     <main>
       <div id="employee-div" className="container">
         <h1>Current Employees</h1>
-        <table id="employee-table" className="display"></table>
+        {/* <table id="employee-table" className="display"></table> */}
+        <DataTable 
+          columns={columns}
+          data={employeeList}
+        />
         <Link to="/">Home</Link>
       </div>
     </main>
